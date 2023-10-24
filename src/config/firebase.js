a02f5270e56a2caa46ce55317da0017f99067414
gabrield14s <126288308+gabrield14s@users.noreply.github.com>
2023-10-24 18:40:06 +0000

    commit1

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,1 +1,12 @@
-import firebase from 'firebase'
+import firebase from 'firebase';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCydDfQf65BoKp7SC5lNMPgyIaIwPf6xdw",
+    authDomain: "login-e085d.firebaseapp.com",
+    projectId: "login-e085d",
+    storageBucket: "login-e085d.appspot.com",
+    messagingSenderId: "138992350553",
+    appId: "1:138992350553:web:5bb4a68683d82e98184848",
+    measurementId: "G-MHS9Z9718L"
+};
